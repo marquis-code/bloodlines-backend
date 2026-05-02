96d69369ad44e3dcb7e5df11b0da210b1bca409b
Toyin <toyinmuhammed50@gmail.com>
2026-05-02 16:36:47 +0100

    feat: include genotype and bloodgroup on signup

--- src/modules/auth/dtos/signup.dto.ts
+++ src/modules/auth/dtos/signup.dto.ts
@@ -1,34 +1,47 @@
-import { IsEmail, IsString, MinLength, Matches, IsEnum } from "class-validator"
-import { ApiProperty } from "@nestjs/swagger"
-import { Gender } from "../../../common/enums/gender.enum"
+import { IsEmail, IsString, MinLength, Matches, IsEnum } from "class-validator";
+import { ApiProperty } from "@nestjs/swagger";
+import { Gender } from "../../../common/enums/gender.enum";
+import { Genotype } from "../../../common/enums/genotype.enum";
+import { BloodGroup } from "../../../common/enums/blood-group.enum";
 
 export class SignupDto {
   @ApiProperty({ example: "John Doe" })
   @IsString()
-  fullName: string
+  fullName: string;
 
   @ApiProperty({ enum: Gender, example: Gender.MALE })
   @IsEnum(Gender)
-  gender: Gender
+  gender: Gender;
 
   @ApiProperty({ example: "+2348012345678" })
   @IsString()
-  phoneNumber: string
+  phoneNumber: string;
 
   @ApiProperty({ example: "john@example.com" })
   @IsEmail()
-  email: string
+  email: string;
+
+  @ApiProperty({ enum: Genotype, example: Genotype.AA })
+  @IsEnum(Genotype)
+  genotype: Genotype;
+
+  @ApiProperty({ enum: BloodGroup, example: BloodGroup.O_POSITIVE })
+  @IsEnum(BloodGroup)
+  bloodGroup: BloodGroup;
 
   @ApiProperty({ example: "Pass123!@#" })
   @IsString()
   @MinLength(8, { message: "Password must be at least 8 characters" })
-  @Matches(/[A-Z]/, { message: "Password must contain at least one uppercase letter" })
+  @Matches(/[A-Z]/, {
+    message: "Password must contain at least one uppercase letter",
+  })
   @Matches(/[0-9]/, { message: "Password must contain at least one number" })
-  @Matches(/[!@#$%^&*]/, { message: "Password must contain at least one special character" })
-  password: string
+  @Matches(/[!@#$%^&*]/, {
+    message: "Password must contain at least one special character",
+  })
+  password: string;
 
   @ApiProperty({ example: "Pass123!@#" })
   @IsString()
-  confirmPassword: string
+  confirmPassword: string;
 }
-
